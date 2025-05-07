7db141e3bf27fd42af7d7cb5dc74e52b969587af
hoffmantim <hoffmantim@me.com>
2025-05-07 11:19:35 -0500

    finish death animation

--- fps-shooter/scripts/skeleton_mage.gd
+++ fps-shooter/scripts/skeleton_mage.gd
@@ -1,9 +1,12 @@
 extends Node3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var despawn_animation_player: AnimationPlayer = $DespawnAnimationPlayer
 
 
 
 func _on_area_3d_body_part_hit(dam: Variant) -> void:
 	print('hit me')
 	animation_player.play("Death_B")
+	await get_tree().create_timer(6).timeout
+	queue_free()
